1f04fe2f574c6dfb586056e9e8fd993941151e11
Vaan W Waber <vaanw@proton.me>
2023-03-28 15:38:19 -0700

    Add DataStore and ProtoBuff
    Update dependencies and Gradle

---
+++ app/src/main/java/com/copixelate/data/proto/UserSettingsSerializer.kt
@@ -0,0 +1,33 @@
+package com.copixelate.data.proto
+
+import android.content.Context
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.Serializer
+import androidx.datastore.dataStore
+import com.copixelate.UserSettings
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+
+object UserSettingsSerializer : Serializer<UserSettings> {
+    override val defaultValue: UserSettings = UserSettings.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): UserSettings {
+        try {
+            return UserSettings.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(
+        t: UserSettings,
+        output: OutputStream
+    ) = t.writeTo(output)
+}
+
+val Context.settingsDataStore: DataStore<UserSettings> by dataStore(
+    fileName = "user_settings.pb",
+    serializer = UserSettingsSerializer
+)
