1636a5c7c1a2ef5b128cd021ec6d617da194a483
Vaan W Waber <vaanw@proton.me>
2023-03-30 18:44:54 -0700

    Finish Settings Screen data access implementation

--- app/src/main/java/com/copixelate/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/com/copixelate/viewmodel/SettingsViewModel.kt
@@ -4,12 +4,20 @@ import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.copixelate.ThemeType
 import com.copixelate.data.SettingsRepo
 import com.copixelate.data.proto.settingsDataStore
+import kotlinx.coroutines.launch
 
 class SettingsViewModel(private val repo: SettingsRepo) : ViewModel() {
+
+    val settings = repo.settingsFlow
+    fun setTheme(type: ThemeType) = viewModelScope.launch {
+        repo.setTheme(type)
+    }
 
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
