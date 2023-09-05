c7a6576bebda63372d3673e31765fa7f8f3b82de
Vaan W Waber <vaanw@proton.me>
2023-09-05 16:54:18 -0700

    Add nav state restoration

--- app/src/main/java/com/copixelate/ui/nav/NavSugar.kt
+++ app/src/main/java/com/copixelate/ui/nav/NavSugar.kt
@@ -5,13 +5,19 @@ import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 
-fun NavController.navigate(navInfo: NavInfo.Screen): Unit =
-    navigate(route = navInfo.route)
+fun NavController.navigate(navInfo: NavInfo.SubScreen): Unit =
+    navigate(route = navInfo.route){
+        launchSingleTop = true
+        restoreState = true
+    }
 
 fun NavController.navigateTopLevel(navInfo: NavInfo.Screen): Unit =
     navigate(navInfo.route) {
-        popUpTo(graph.findStartDestination().id)
+        popUpTo(graph.findStartDestination().id) {
+            saveState = true
+        }
         launchSingleTop = true
+        restoreState = true
     }
 
 fun NavController.compareRoute(navInfo: NavInfo.Screen): Boolean =
