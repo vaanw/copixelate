e2306b111b41f896e574a9e6ef0e3f63f56f925d
Vaan Waber <vaanw@proton.me>
2023-10-07 17:59:06 -0700

    Improve brush preview

--- art/src/main/java/com/copixelate/art/Point.kt
+++ art/src/main/java/com/copixelate/art/Point.kt
@@ -2,22 +2,20 @@ package com.copixelate.art
 
 import kotlin.math.floor
 import kotlin.math.min
+import kotlin.math.sqrt
 
-data class Point(var x: Int = 0, var y: Int = x) {
+data class Point(val x: Int = 0, val y: Int = x) {
     operator fun div(i: Int) = Point(x / i, y / i)
     operator fun div(f: Float) = PointF(x / f, y / f)
     operator fun div(p: Point) = PointF(x * 1f / p.x, y * 1f / p.y)
-
-    operator fun plusAssign(i: Int) {
-        x += i; y += i
-    }
+    operator fun plus(i: Int) = Point(x + i, y + i)
 
     val area get() = x * y
     fun contains(p: PointF): Boolean = (p.x > 0 && p.x < x && p.y > 0 && p.y < y)
-    fun toMinSquare() = Point(min(x, y))
+    fun toMinSquareRoot() = Point(sqrt(min(x, y).toDouble()).toInt())
 }
 
-data class PointF(var x: Float = 0f, var y: Float = x) {
+data class PointF(val x: Float = 0f, val y: Float = x) {
     operator fun times(f: Float) = PointF(f * x, f * y)
     operator fun times(p: PointF) = PointF(x * p.x, y * p.y)
     operator fun times(p: Point) = PointF(x * p.x, y * p.y)
